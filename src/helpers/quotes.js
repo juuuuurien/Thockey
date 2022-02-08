08573a925d24ce8188becec3e27d49cba8da7f6b
juuuuurien <97487768+juuuuurien@users.noreply.github.com>
2022-02-07 17:16:01 -0800

    update quotes to objects with author

--- src/helpers/quotes.js
+++ src/helpers/quotes.js
@@ -1,5 +1,16 @@
 const gameQuotes = [
   "That upsmash. Oh Zhu- poor Zhu. Happy feet. Wombo combo. That ain't Falco. That ain't falc-oh. OH! OOH! OOOOH! OOOH! OOOH! OOOH! WHERE ARE YOU AT?! WHERE ARE YOU AT?! WHERE ARE YOU AT?! WHERE ARE YOU AT?! OOOHH. OH MY GOD. WOMBO COMBO. GET YO ASS WOOPED",
   "Rare footage of Daigo actually angry.",
-  "Hello, hello? Uh, I wanted to record a message for you to help you get settled in on your first night. Um, I actually worked in that office before you. I'm finishing up my last week now, as a matter of fact. So, I know it can be a bit overwhelming, but I'm here to tell you there's nothing to worry about. Uh, you'll do fine. So, let's just focus on getting you through your first week.",
+  {
+    author: `Five Nights at Freddy's`,
+    quotes:
+      "Hello, hello? Uh, I wanted to record a message for you to help you get settled in on your first night. Um, I actually worked in that office before you. I'm finishing up my last week now, as a matter of fact. So, I know it can be a bit overwhelming, but I'm here to tell you there's nothing to worry about. Uh, you'll do fine. So, let's just focus on getting you through your first week.",
+  },
+  {
+    author: "Andrew Ryan",
+    quotes: [
+      " I chose the impossible. I chose...Rapture, a city where the artist would not fear the censor, where the scientist would not be bound by petty morality, Where the great would not be constrained by the small! And with the sweat of your brow, Rapture can become your city as well.",
+      "I am Andrew Ryan, and I'm here to ask you a question. Is a man not entitled to the sweat of his brow? 'No!' says the man in Washington, 'It belongs to the poor.'",
+    ],
+  },
 ];
