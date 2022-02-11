c0193b4a970b6aa19e714caa3361fa7513bcff52
juuuuurien <97487768+juuuuurien@users.noreply.github.com>
2022-02-11 14:00:55 -0800

    added facts and quotes

---
+++ public/funfacts.js
@@ -0,0 +1,17 @@
+export const funfacts = [
+  "Ditto was meant to be usable through pokeballs in Super Smash Bros. Melee. It was meant to transform into whoever threw the ball and would help fight. The pokemon was shown in Melee's official strategy guide, suggesting it was removed late into development.",
+  "Looking into the assets of Super Smash Bros. Melee, you will find that the image reflecting on the metroid trophy is a part of the Super Metroid title screen.",
+  "In Super Smash Bros. Melee, it you set the system language to Japanese, Mewtwo will speak in fulls entences during its victory poses. When set to English, Mewtwo only scoffs and laughs.",
+  "Hideo Kojima has requested that Nintendo have Solid Snake in Super Smash Bros. Melee, but due to time constraints, he couldn't be added to the game. Sonic also nearly made it into the game, but was cut due to time constraints as well.",
+  "While developing Super Smash Bros. Melee, creator Masahiro Sakurai only took one day off of work and even fainted while working on the game's musical composition.",
+  "In Super Mario Sunshine, Il Pianticcimo, a masked human disguised as a Pianta, is an npc Mario is able to race throughout the game. Hacking the game's ingame model and removing his mask reveals he is actually the running man from Ocarina of Time and Majoras Mask!",
+  "Mario's very first appearance was not in a Super Mario game, but rather the 1981 arcade platformer, Donkey Kong.",
+  "Bowser from the Super Mario series was originally sketched up as an ox by creator Shigeru Miyamoto, but his drawing were misinterpreted by animator Yoichi Kotabe as a turtle.",
+  "In 1993, and after the release of the Super Mario Bros. movie, two porn films titled Super Hornio Brothers and Super Hornio Brothers II were released. Both movies starred porn legend Ron Jeremy as Mario. Nintendo bought the rights to both films and buried them to prevent further distribution.",
+  "Koji Kondo is one of the most famous video game composers for his work on all the previous Legend of Zelda games. However, Breath of the Wild is the first Zelda game title without his work.",
+  "Satoru Iwata was the fourth president and CEO of Nintendo and passed away in 2015. In The Legend of Zelda Breath of the Wild, not only is Satori Mountain named after him, but the developers designed this mountaintop to be one of the most beautiful areas in the game.",
+  "It is known that Breath of the Wild's Hyrule is insanely large, and in comparison to Twilight Princess' Hyrule, it is actually 12 times larger.",
+  "In The Legend of Zelda Ocarina of Time, there is a glitch that allows the Link to warp directly from the Great Deku Tree to escaping Ganon's castle, allowing you to beat the game in under 25 minutes.",
+  "The overworld theme for World 3 of Super Mario Bros. 3 is actually the predecessor to the Great Fairy Fountain theme from The Legend of Zelda.",
+  "In the Legend of Zelda Wind Waker, there is concept art of an adult Link in various action poses. This could suggest Link's age was supposed to change throughout the game similar to Ocarina of Time. ",
+];
