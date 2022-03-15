7862f2a35a0e35e53af757d9e9fdffad289429ea
juuuuurien <97487768+juuuuurien@users.noreply.github.com>
2022-03-14 17:37:01 -0700

    Updates

---
+++ precache-manifest.845178da7780e726f9b7a5ecabcb41a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf64b9be78821ce24d13",
+    "url": "/static/css/main.fc901497.chunk.css"
+  },
+  {
+    "revision": "cf64b9be78821ce24d13",
+    "url": "/static/js/main.ecf698de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b9d7702f890fe09ddc47",
+    "url": "/static/js/2.3b29510d.chunk.js"
+  },
+  {
+    "revision": "fce65fbc68cd62024c04ce460f01b489",
+    "url": "/index.html"
+  }
+];
